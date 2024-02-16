1e129eed9eecaa8f8347f6b7638ddc41a5ae5739
OmkarBhosale07 <omkarbhosale5484@gmail.com>
2024-02-16 21:52:29 +0530

    code update new

--- src/data/featured.js
+++ src/data/featured.js
@@ -1,15 +1,12 @@
 export const FeaturedProjectData =[{
-<<<<<<< HEAD
     title: "Keep It Secret",
     para: "It is the secret message sharing platform.",
     img: "https://media.discordapp.net/attachments/1066265844167999488/1170719342023950376/Home-screen.jpg?ex=655a105a&is=65479b5a&hm=f3c64adf7ac67b9cb4e9d6c55477a8bd0bbb54895ff36b6d65380f61fd5f28dc&=&width=1231&height=876",
     tech:["NextJs", "Tailwind CSS"],
-=======
     title: "Keep It Secret 🤫",
     para: "A secret message sharing platform with message encryption and decryption option!",
     img: "/",
     tech:["NextJs 14", "Tailwind CSS","Crypto-JS"],
->>>>>>> 8e1cf0f03f17440e2412e438496f012de7a37e23
     link: "https://keep-it-secret.vercel.app/",
     github:"https://github.com/0mkarBhosale07/keep-it-secret",
   },
